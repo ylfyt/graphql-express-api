445c752f3e2b19a6cb27e6ca14cc5896fb633631
yudialfayat <ayudi3521@gmail.com>
2021-12-04 01:46:38 +0700

    adding author. But type bug

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,16 +1,34 @@
-import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLInt, GraphQLFloat } from 'graphql';
+import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLInt, GraphQLList } from 'graphql';
 import _ from 'lodash';
 
-import { books } from '../db/books';
+import { authors, books } from '../db/books';
 
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	fields: () => ({
 		id: { type: GraphQLID },
 		title: { type: GraphQLString },
-		edition: { type: GraphQLString },
 		year: { type: GraphQLInt },
-		price: { type: GraphQLFloat },
+		author: {
+			type: AuthorType,
+			resolve: (parent, args) => {
+				return _.find(authors, { id: parent.authorId });
+			},
+		},
+	}),
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		books: {
+			type: new GraphQLList(BookType),
+			resolve: (parent, args) => {
+				return _.filter(books, { authorId: parent.id });
+			},
+		},
 	}),
 });
 
@@ -22,6 +40,13 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: { type: GraphQLID } },
 			resolve: (parent, args) => {
 				return _.find(books, { id: args.id });
+			},
+		},
+		author: {
+			type: AuthorType,
+			args: { id: { type: GraphQLID } },
+			resolve: (parent, args) => {
+				return _.find(authors, { id: args.id });
 			},
 		},
 	},
